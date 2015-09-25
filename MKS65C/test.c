857006c4ad79fcbba52bfbc2ef0bfed57ca4ac31
daltonwu <dalton.z.wu@gmail.com>
2015-09-25 14:07:14 -0400

    Update

--- MKS65C/test.c
+++ MKS65C/test.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 
 int main() {
-	while(1) {
-		float f;
-		if(f!=0) {
-			printf("%f", f);
-		}
-		else {
-			f = rand();
-		}
+	int bob = -1;
+	int a[5] = {1, 2, 3, 4, 5};
+	printf("bob = %d\n", bob);
+	int i;
+	for(i=0; i<5; i++) {
+		printf("a[%d] = %d\n", i, a[i]);
+	}
+	i=0;
+	for(i=0; i>=-5; i--) {
+		printf("a[%d] = %d\n", i, a[i]);
 	}
 	return 0;
 }
