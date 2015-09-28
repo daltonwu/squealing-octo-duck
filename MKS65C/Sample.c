6c1ef470c6892da6a6da0085c91724acf9d1d153
daltonwu <dalton.z.wu@gmail.com>
2015-09-28 14:02:47 -0400

    Update

--- MKS65C/Sample.c
+++ MKS65C/Sample.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "mylib.h"
 
 int main() {
-	int bob = 7;
-	printf("Hello, world!\n");
-	printf("bob = %d\n", bob);
+	int i;
+	double d;
+	char c;
+
+	int *I = &i;
+	double *D = &d;
+	char *C = &c;
+	
+	printf("%lu\n%lu\n%lu\n\n", I, D, C);
+	
+	I++;
+	D++;
+	C++;
+	
+	printf("%lu\n%lu\n%lu\n\n", I, D, C);
+	
 	return 0;
 }
