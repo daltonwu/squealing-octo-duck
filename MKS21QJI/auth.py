95b22322d6d6b09332f9ae03babd58dc12dfc12a
daltonwu <dalton.z.wu@gmail.com>
2015-11-23 12:31:02 -0500

    Update

---
+++ MKS21QJI/auth.py
@@ -0,0 +1,10 @@
+def legal_password(s):
+    """tests to see if a password s is legally formed
+    
+    Arguments:
+      s: a string representing a possible password
+    
+    Return:
+      True if s is a legally formed password, False otherwise
+    """
+    pass
