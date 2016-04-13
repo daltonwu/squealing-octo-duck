2109a2c39b7892d12d82964778fcd2d1a98d8455
daltonwu <dalton.z.wu@gmail.com>
2016-04-13 13:17:21 -0400

    :non-potable_water:

---
+++ MKS22QJI/16-04-13/pumpkin.py
@@ -0,0 +1,33 @@
+import time
+
+val = [1, 1]
+
+def timer(f):
+    def inner(*arg):
+        t0 = time.time()
+        foo = f(*arg)
+        t1 = time.time()
+        print f.__name__ + str(arg) + ': ' + str(t0 - t1)
+        return foo
+    return inner
+
+def remember(f):
+    values = {}
+    def inner(arg):
+        if arg not in values:
+            values[arg] = f(arg)
+        return values[arg]
+    return inner
+
+@timer
+def fib(n):
+    global val
+    while len(val) < n: val.append(val[-1] + val[-2])
+    return val[n-1]
+
+@timer
+@remember
+def fib2(n):
+    return 1 if n <= 2 else fib(n-1) + fib(n-2)
+
+print fib2(30)
